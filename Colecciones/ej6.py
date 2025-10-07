913fa30f8674cd7b0442dcd2e0b1ffefbfcd629c
LauraLuzhen <ll.rodriguez@iesnervion.es>
2025-10-07 14:43:01 +0200

    ej

--- Colecciones/ej6.py
+++ Colecciones/ej6.py
@@ -1,4 +1,5 @@
-# Escribe un programa que tome una cadena de texto como entrada y genere un diccionario que cuente la frecuencia de cada palabra en el texto.
+# Escribe un programa que tome una cadena de texto como entrada y genere un diccionario que cuente la frecuencia de 
+# cada palabra en el texto.
 
 texto = str(input('Introduce un texto:'))
 
